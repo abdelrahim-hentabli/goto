689668d6db7b6febfb5334356bbf1b6e45e289ad
Abdelrahim Hentabli <abdelrahim.hentabli@gmail.com>
2019-12-01 21:05:03 +0000

    renamed file to fix conflicts with importing library

---
+++ times.py
@@ -0,0 +1,48 @@
+from datetime import datetime
+
+def julianDate():
+    now = datetime.utcnow()
+    month = now.month
+    year = now.year
+    day = now.day
+    if(month <= 2):
+        y-= 1
+        m += 12
+    a = year //100
+    b = a//4
+    c = 2-a+b
+    e = int(365.25*(year +4716))
+    f = (int)(30.6001 * (month +1))
+    return c+day+e+f- 1524.5
+
+def GreenwichMeanSiderialTime():
+    d = julianDate() - 2451545.0    #Days since 2000 Jan. 1 12H UT1
+    T = d / 36525.0;                #Centuries since 2000 Jan. 1 12H UT1
+    now = datetime.utcnow()
+    GMST = 6.697374558 + .06570982441908 * d + 1.00273790935 * (now.hour + (now.minute/60) + (now.second/3600)) + .000026 * (T**2)
+    return GMST - ((GMST // 24) * 24)
+
+def getSiderialTime():
+    gst = GreenwichMeanSiderialTime()
+    siderealHour = int(gst)
+    remainder = (gst- siderealHour) * 60
+    siderialMinute = int(remainder)
+    siderialSecond = int( (remainder - siderialMinute) * 60)
+    return (siderealHour, siderialMinute, siderialSecond)
+
+def getLocalSiderialTime(longitude):
+    gst = GreenwichMeanSiderialTime()
+    lst = (gst + longitude /15) % 24
+    siderealHour = int(lst)
+    remainder = (lst- siderealHour) * 60
+    siderialMinute = int(remainder)
+    siderialSecond = int( (remainder - siderialMinute) * 60)
+    return (siderealHour, siderialMinute, siderialSecond)
+
+def getLocalSiderialTimeAngle(longitude):
+    gst = GreenwichMeanSiderialTime()
+    lst = ((gst + longitude /15) % 24 )* 15
+    return lst
+
+
+#print(getLocalSiderealTime(-117.323194))
