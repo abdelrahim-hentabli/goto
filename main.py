24c5ee31b5d4e198fc5ecbd12efd932f346059c5
Abdelrahim Hentabli <abdelrahim.hentabli@gmail.com>
2019-12-03 08:32:54 +0000

    finished goto

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 from gps import get_gps
-from accel_mag import get_accel_mag 
-import geomag as gm
-from time import sleep
-from math import atan, degrees
 from stars import getAltAz
 from times import getLocalSiderialTimeAngle
+from motor import calibrate, gotoAltAzimuth
 
 File = open('TopStars.txt', 'r')
-
 topStars = []
 topStarsByName = {}
 
@@ -18,49 +14,17 @@ for line in File:
         topStarsByName[split[2]] = (split[1],split[2],15 * (float(split[3]) + float(split[4])/60), float(split[5]),float(split[6]))
 
 
-
-
 #latitude, longitude = get_gps()
+latitude = 33.8953781
+longitude = -117.3231389
 
-#magDec = gm.declination(latitude, longitude)
-magDec = gm.declination(33.975599,-117.326188)
+calibrate(latitude, longitude)
 
-
-
-'''
-while True: 
-    accel_mag = get_accel_mag()
-    acceleration = accel_mag["acceleration"]
-    magnetic = accel_mag["magnetic"]
-    while(acceleration[0] **2 + acceleration[1] ** 2 + acceleration[2] **2 < 56.25 or acceleration[0] **2 + acceleration[1] ** 2 + acceleration[2] **2 > 156.25  ):
-        accel_mag = get_accel_mag()
-        acceleration = accel_mag["acceleration"]
-        magnetic = accel_mag["magnetic"]
-    if(magnetic[0] != 0):
-        magNorth = degrees(atan(magnetic[1]/magnetic[0]))
-        if(magnetic[0] < 0):
-            magNorth += 180
-        if(magNorth < 0):
-            magNorth += 360
-    else:
-        if(magnetic[1] < 0):
-            magNorth = 270
-        else:
-            magNorth = 90
-
-    trueNorth = magNorth + magDec
-    if(trueNorth < 0):
-        trueNorth += 360
-    elif(trueNorth > 360):
-        trueNorth -= 360
-    print("Magnetic North Angle from +X", magNorth)
-    print("True North Angle from +X", trueNorth)
-    print("\n\n")
-    sleep(1)
-'''
-
-while True:
+exit = False
+while not exit:
     temp = input('Which star do you wanna see?:  ')
+    if temp == 'exit':
+        exit = True
     star = None
     try:
         starNumber = int(temp)
@@ -69,6 +33,6 @@ while True:
         if temp in topStarsByName:
             star = topStarsByName[temp]
     if star is not None:
-        print(getAltAz(34.1478, getLocalSiderialTimeAngle(-118.1268), star[2], star[3]))
-
-
+        alt, az =getAltAz(latitude, getLocalSiderialTimeAngle(longitude), star[2], star[3])
+        gotoAltAzimuth(alt, az)
+gotoAltAzimuth(90 , 0)
